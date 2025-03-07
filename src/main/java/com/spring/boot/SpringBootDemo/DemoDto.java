b7bd046016e165794ab5693ce415fd1dd0b42b15
Chaitanya-vengal-alt <chaitanyavengal9@gmail.com>
2025-03-07 11:58:10 +0530

    created dto class for rough

---
+++ src/main/java/com/spring/boot/SpringBootDemo/DemoDto.java
@@ -0,0 +1,34 @@
+package com.spring.boot.SpringBootDemo;
+
+public class DemoDto {
+	
+	private String name;
+	private String add;
+	private int id;
+	private double sal;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdd() {
+		return add;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+
+}
